4e5c868fbc743b80ea5e8cc7a9b40a4e142357a5
Suchitra Naik <susanaik@gmail.com>
2018-07-11 12:17:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ function fuzzyMatch(drivers, name){
 
 function matchName(drivers, user){
   return drivers.filter(function (driver) {
-   return driver.toLowerCase() === user.toLowerCase();
+   return driver.name === user.toLowerCase();
  });
 }
