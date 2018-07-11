e61e7bb5a2b824eb65c8544c9a15a4edfb654beb
Suchitra Naik <susanaik@gmail.com>
2018-07-11 11:14:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file
+drivers ={};
+
+function findMatching(drivers, name){
+  drivers.filter(function (driver) { return driver == name; });
+}
+
+function fuzzyMatch(drivers, string){
+
+
+}
+
+function matchName(drivers, string){
+
+}
